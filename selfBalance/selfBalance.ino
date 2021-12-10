870b328d1841055784113af7f6fae3af3488aafa
Nicolas Garzione <nicg1808@gmail.com>
2021-12-09 20:01:11 -0700

    REV 1

--- selfBalance/selfBalance.ino
+++ selfBalance/selfBalance.ino
@@ -22,12 +22,27 @@ int motSpeed = 0;
 
 /* Variables neeeded for IMU */
 const int MPU=0x68;
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+int16_t ax, ay, az, gx, gy, gz;
+
+/* Bias variables */
+float accBiasX, accBiasY, accBiasZ;
+float gyroBiasX, gyroBiasY, gyroBiasZ;
+
+/* Variables to store MPU data */
+float accAngleX, accAngleY;
+double accPitch, accRoll;
+
+float gyroRateX, gyroRateY, gyroRateZ;
+float gyroBias_oldX, gyroBias_oldY, gyroBias_oldZ;
+float gyroPitch = 180;
+float gyroRoll = -180;
+float gyroYaw = 0;
 
 /* Variables needed for PID controller */
 double convertVal = 0.001;
 double Input, Output, Setpoint;
-double Kp=30, Ki=100, Kd=0;
+double Kp=16, Ki=3, Kd=.2;
+// 16, 0, 0
 
 /* Initializes PID function with above variables */
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
@@ -36,6 +51,12 @@ PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 bool flip = 0;
 bool store = 0;
 bool flipdirection = 0;
+
+/* timer */
+uint32_t timer;
+
+/* Complimentary filter variable */
+double filtered = 0;
 
 /* Initial setup */
 void setup()
@@ -68,34 +89,56 @@ void setup()
     /* Sets PID controller to automatic (computes each time) */
     myPID.SetMode(AUTOMATIC);
 
-    /* Calibration value obtained from testing */
-    mpu.setYAccelOffset(-1243);
+    /* Calibration values obtained from testing */
+    mpu.setXAccelOffset(-2072);
+    mpu.setYAccelOffset(-1287);
+    mpu.setZAccelOffset(965);
+    mpu.setXGyroOffset(-60);
+    mpu.setYGyroOffset(-501);
+    mpu.setZGyroOffset(-537);
+  
+    gyroBiasX = 0;
+    gyroBiasY = 0;
+    gyroBiasZ = 1;
+  
+    accBiasX = 6;
+    accBiasY = 21;
+    accBiasZ = 16381;
+
+    /* Pulls MPU data */
+    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    /* Calculates initial values for Complimemtary filter */
+    accPitch = (atan2(-ax, -az) + PI) * RAD_TO_DEG;
+    accRoll = (atan2(ay, -az) + PI) * RAD_TO_DEG;
+
+    if (accPitch <= 360 & accPitch >= 180) {
+      accPitch = accPitch - 360;
+    }
+
+    if (accRoll <= 360 & accRoll >= 180) {
+      accRoll = accRoll - 360;
+    }
+
+    gyroPitch = accPitch;
+    gyroRoll = accRoll;
+
+    timer = micros();
+    delay(1000);
 }
 
 void loop()
 {   
-    /* Stores data from IMU */
-    /*
-    Wire.beginTransmission(MPU);
-    Wire.write(0x3B);
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU,14,true);
-    AcX=Wire.read()<<8|Wire.read();
-    AcY=Wire.read()<<8|Wire.read();
-    AcZ=Wire.read()<<8|Wire.read();
-    Tmp=Wire.read()<<8|Wire.read();
-    GyX=Wire.read()<<8|Wire.read();
-    GyY=Wire.read()<<8|Wire.read();
-    GyZ=Wire.read()<<8|Wire.read(); 
-    */
-
     /* Library utilized to read MPU data */
-    AcY = mpu.getAccelerationY(); 
+    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-    //Serial.println(AcY); 
+    /***********************
+     * Complimentary filter
+     ***********************/
+    filtered = complimentaryFilter(ay, az, gx);
 
     /* Measures which way robot is leaning */
-    if (AcY > 0)
+    if (filtered > 0)
     {
       flipdirection = 0;
     }
@@ -116,28 +159,35 @@ void loop()
     }
 
     /* Changes error to a value PID controller can work with */
-    Input = -abs(convertVal * AcY);
-    //Input = convertVal * AcY;
+    Input = -abs(filtered);
 
-    /* Computes PID output to change motor speed */
+    /*****************
+     * PID Controller
+     *****************/
     myPID.Compute(flip);
 
     /* Determines direction of motors and inputs to motor function */
-    if (AcY < 0)
+    if (filtered > 0)
     {
       driveControl(1, Output);
-      //Serial.println(convertVal * AcY);
+      Serial.print(millis());
+      Serial.print(';'); 
+      Serial.print(filtered);
+      Serial.print(';');
       Serial.println(Output); 
-    }
-    else if (AcY > 0)
-    {
-      driveControl(1, -Output);
-      //Serial.println(convertVal * AcY);
-      Serial.println(-Output); 
+      delay(.05);
+      driveControl(0, Output);
     }
     else
     {
-      driveControl(0, Output);
+      driveControl(1, -Output);
+      Serial.print(millis());
+      Serial.print(';'); 
+      Serial.print(filtered);
+      Serial.print(';');
+      Serial.println(-Output);
+      delay(.05);
+      driveControl(0, Output); 
     }
     
     //driveControl(1,Output);
@@ -178,7 +228,6 @@ void motorControl(int motnum, int input)
     /* Left motor control */
     if (motnum == 0)
     {
-      //Serial.print("0");
       analogWrite(MOT_0_PWM, abs(input));
       if (input >= 0)
       {
@@ -194,7 +243,6 @@ void motorControl(int motnum, int input)
     /* Right motor control */
     else if (motnum == 1)
     {
-      //Serial.print("1");
       analogWrite(MOT_1_PWM, abs(input));
       if (input >= 0)
       {
@@ -211,11 +259,27 @@ void motorControl(int motnum, int input)
     {
       //nothing
     }
+}
 
-    //Serial.println("Recieved");
-    
-    //Serial.print("Motor ");
-    //Serial.print(motnum);
-    //Serial.print(" at power: ");
-    //Serial.println(abs(input));
+/* complimentaryFilter(...) *******************************************************
+ *     This is a complimentary filter, a computationally inexpensive sensor fusion 
+ *     technique that consists of a low-pass and a high-pass filter. In this use,
+ *     it allows the MPU data to be filtered and avoid noise and overcorrection
+ *     by th PID controller.
+ **********************************************************************************/
+double complimentaryFilter(int16_t ay, int16_t az, int16_t gx){
+    accRoll = (atan2(ay/182.0, -az/182.0) + PI) * RAD_TO_DEG;
+
+    if (accRoll <= 360 & accRoll >= 180) {
+      accRoll = accRoll - 360;
+    }
+
+    gyroRateX = -((int)gx - gyroBiasX) / 131; 
+
+    double gyroVal = gyroRateX * ((double)(micros() - timer) / 1000000);
+
+    timer = micros();
+
+    /* Complementary filter value */
+    return 0.98 * (filtered + gyroVal) + 0.02 * (accRoll);
 }
